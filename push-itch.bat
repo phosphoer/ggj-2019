59f89cf33b9d613bf135f59057b3f71758ef3736
David Evans <david0evans@gmail.com>
2019-08-10 11:25:04 -0700

    add windows script to push to itch

---
+++ push-itch.bat
@@ -0,0 +1,3 @@
+butler push build/deco-dash-mac/ stickybiscuits/deco-dash:osx
+butler push build/deco-dash-windows/ stickybiscuits/deco-dash:windows
+butler push build/deco-dash-linux/ stickybiscuits/deco-dash:linux
