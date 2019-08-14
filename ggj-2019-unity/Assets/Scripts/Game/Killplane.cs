bd7060b788477d7d6cd0217cb3ec8bc14f7bfbd1
Jeff Sparks <jeff-sparks@hotmail.com>
2019-08-14 22:10:23 +0100

    Killplane added
    Addresses objects falling out of the world by adding a plane below the level that respawns items above the center of the level

---
+++ ggj-2019-unity/Assets/Scripts/Game/Killplane.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killplane : MonoBehaviour
+{
+	public Transform respawn;
+	
+	void OnCollisionEnter(Collision col)
+	{
+		col.gameObject.transform.position = respawn.transform.position;
+		col.rigidbody.velocity = Vector3.zero;
+		col.rigidbody.angularVelocity = Vector3.zero;
+	}
+}
